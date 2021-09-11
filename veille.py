1ffb90fbd2d9d209e5e20b9bef09cedfda1db517
Pravdin Maksym <maks111111112@gmail.com>
2021-09-11 16:30:39 -0400

    Ajout de nouvelles categories de cartes et leur suppression et fix si la liste est vide

--- veille.py
+++ veille.py
@@ -4,6 +4,8 @@ def afficher_menu():
     print("L: Lire une carte / afficher les informations")
     print("N: Ajouter une nouvelle catégorie de cartes")
     print("M: Modifier le contenu d'une carte")
+    print("C: Modifier le nom d'une catégorie de cartes")
+    print("D: Supprimer une catégorie de cartes")
     print("Q: Quitter l'application")
 
 
@@ -15,82 +17,115 @@ def afficher_listes():
 
 
 def afficher_listes_dictionnaire():
-    afficher_listes()
-
-    input_menu_read = input("Quel liste voulez vous afficher? ")
-    afficher_contenu_liste(input_menu_read)
+    if category_dict:
+        afficher_listes()
+        input_menu_read = input("Quel liste voulez vous afficher? ")
+        afficher_contenu_liste(input_menu_read)
+    else:
+        print("Il n'y a pas de listes")
 
 
 def afficher_contenu_liste(input_menu_read):
     try:
-        counter = 0
-        for story in category_dict[input_menu_read]:
-            print("     - {} ({})".format(story, counter))
-            counter += 1
+        if category_dict[input_menu_read]:
+            counter = 0
+            for story in category_dict[input_menu_read]:
+                print("     - {} ({})".format(story, counter))
+                counter += 1
+        else:
+            print("La liste est vide!")
     except KeyError:
         print("Clé invalide! Retour au menu")
 
 
 def ajouter_story_dans_liste():
-    afficher_listes()
+    if category_dict:
+        afficher_listes()
 
-    input_story = input("Entrez votre story à ajouter: ")
+        input_story = input("Entrez votre story à ajouter: ")
 
-    input_menu_read = input("Dans quelle liste voulez vous ajouter la story? ")
-    try:
-        category_dict[input_menu_read].append(input_story)
-        print("Story ajoutée!!!")
-    except KeyError:
-        print("Clé invalide! Retour au menu....")
+        input_menu_read = input("Dans quelle liste voulez vous ajouter la story? ")
+        try:
+            category_dict[input_menu_read].append(input_story)
+            print("Story ajoutée!!!")
+        except KeyError:
+            print("Clé invalide! Retour au menu....")
+    else:
+        print("Il n'y a pas de listes")
 
 
 def supprimer_story_dans_liste():
-    afficher_listes()
+    if category_dict:
+        afficher_listes()
 
-    input_menu_delete = input("Dans quelle liste voulez vous supprimer la story: ")
-    afficher_contenu_liste(input_menu_delete)
+        input_menu_delete = input("Dans quelle liste voulez vous supprimer la story: ")
+        afficher_contenu_liste(input_menu_delete)
 
-    while True:
-        input_story_delete = input("Entrez le numéro de la story que vous voulez supprimer: ")
+        while True:
+            input_story_delete = input("Entrez le numéro de la story que vous voulez supprimer: ")
 
-        try:
-            input_int = int(input_story_delete)
             try:
-                category_dict[input_menu_delete].pop(input_int)
-                print("Story supprimée!!!")
-                break
+                input_int = int(input_story_delete)
+                try:
+                    category_dict[input_menu_delete].pop(input_int)
+                    print("Story supprimée!!!")
+                    break
 
-            except IndexError:
-                print("Erreur! Retour au menu...")
-                break
+                except IndexError:
+                    print("Erreur! Retour au menu...")
+                    break
 
-        except ValueError:
-            print("Cela n'est pas un nombre")
+            except ValueError:
+                print("Cela n'est pas un nombre")
+    else:
+        print("Il n'y a pas de listes")
 
 
 def modifier_story_dans_liste():
-    afficher_listes()
+    if category_dict:
+        afficher_listes()
 
-    input_menu_modif = input("Dans quelle liste se trouve la story à modifier: ")
-    afficher_contenu_liste(input_menu_modif)
+        input_menu_modif = input("Dans quelle liste se trouve la story à modifier: ")
+        afficher_contenu_liste(input_menu_modif)
 
-    while True:
-        input_story_modif = input("Entrez le numéro de la story que vous voulez modifier: ")
-        input_story_content = input("Entrez le nouveau texte à mettre dans la story: ")
+        while True:
+            input_story_modif = input("Entrez le numéro de la story que vous voulez modifier: ")
+            input_story_content = input("Entrez le nouveau texte à mettre dans la story: ")
 
-        try:
-            input_int = int(input_story_modif)
             try:
-                category_dict[input_menu_modif][input_int] = input_story_content
-                print("Story modifiée!!!")
-                break
+                input_int = int(input_story_modif)
+                try:
+                    category_dict[input_menu_modif][input_int] = input_story_content
+                    print("Story modifiée!!!")
+                    break
 
-            except IndexError:
-                print("Erreur! Retour au menu...")
-                break
+                except IndexError:
+                    print("Erreur! Retour au menu...")
+                    break
 
-        except ValueError:
-            print("Cela n'est pas un nombre")
+            except ValueError:
+                print("Cela n'est pas un nombre")
+    else:
+        print("Il n'y a pas de listes")
+
+
+def ajouter_nouvelle_liste():
+    nouvelle_liste_nom = input("Entrez le nom de la nouvelle liste: ")
+    category_dict[nouvelle_liste_nom] = []
+    print("Nouvelle catégorie ajoutée!")
+
+
+def supprimer_liste():
+    if category_dict:
+        afficher_listes()
+        liste_a_supprimer = input("Entrez le nom de la liste de cartes à supprimer: ")
+        try:
+            category_dict.pop(liste_a_supprimer)
+            print("Liste supprimée!!!")
+        except KeyError:
+            print("Le nom de la liste entré n'existe pas!!!")
+    else:
+        print("Il n'y a pas de listes")
 
 
 todo_list = ["Tache2", "Tache3", "Tache4"]
@@ -115,10 +150,16 @@ while True:
         afficher_listes_dictionnaire()
 
     elif input_menu[0] == "N":
-        print("Nouveau")
+        ajouter_nouvelle_liste()
 
     elif input_menu[0] == "M":
         modifier_story_dans_liste()
+
+    elif input_menu[0] == "C":
+        print("Categorie")
+
+    elif input_menu[0] == "D":
+        supprimer_liste()
 
     elif input_menu[0] == "Q":
         break
