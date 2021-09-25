240825524b5e9d2306e71a5f90a3dadfa0544407
Pravdin Maksym <maks111111112@gmail.com>
2021-09-25 16:38:15 -0400

    Ajout de styles au bouton pour quitter

--- veilleGUI.py
+++ veilleGUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import *
+import tkinter.font as font
 
 window = tk.Tk()
 window.title("Trello en Python GUI")
@@ -17,7 +18,7 @@ canvas_titre.place(x=75, y=20)
 titre_label = Label(canvas_titre, text="Trello en Python GUI", bg='#338BA8', fg='#C0C0C0', font="none 52 bold")
 titre_label.place(x=28, y=0)
 
-canvas_principal = Canvas(window, width=750, height=750, bg='#338BA8', highlightthickness=0)
+canvas_principal = Canvas(window, width=750, height=720, bg='#338BA8', highlightthickness=0)
 canvas_principal.place(x=75, y=125)
 
 infos_commandes = Label(canvas_principal, text="↓ Entrez une commande ↓", bg='#338BA8', fg='#C0C0C0',
@@ -27,7 +28,11 @@ infos_commandes.place(x=132, y=0)
 ligne_de_commande = Entry(canvas_principal)
 ligne_de_commande.place(x=132, y=58, width=486, height=25)
 
-# exit_button = Button(canvas_principal, text="Exit", command=window.quit)
-# exit_button.place(x=150, y=500)
+
+
+font_bouton = font.Font(family='Courier', size=20, weight='bold')
+exit_button = Button(window, text='Quitter', bg='#cc1400', fg='#ffffff', borderwidth=0, command=window.quit)
+exit_button['font'] = font_bouton
+exit_button.place(x=380, y=848)
 
 window.mainloop()
